b598c880e2e64df2a8a7063ade1e1d08700a1e24
gracejoh <ganwynj@icloud.com>
2020-07-22 17:20:41 +0000

    .

---
+++ main.py
@@ -0,0 +1,44 @@
+import random 
+
+r = int(random.randint(-100,100))
+
+found = False
+
+
+while found == False :
+  
+  n = int(input("type a number: "))
+ 
+  if n == r :
+    found == True
+    print("correct number")
+  elif n < r :
+    print("the number is too low.")
+  elif n > r :
+    print("the number is too high")
+  else:
+    print ("invalid number")
+
+###
+
+import random 
+
+list_1 = []
+
+r = 
+
+found = False
+
+
+while found == False :
+  
+  n = str(input("type a letter: "))
+ 
+  if n == r :
+    found == True
+    print("correct letter")
+  
+  else:
+    print ("invalid")
+
+
